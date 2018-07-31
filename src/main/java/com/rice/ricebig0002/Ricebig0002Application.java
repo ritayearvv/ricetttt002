df521153236b5b2b7cc9699b2ce414907ca37311
rita <rita@gmal.com>
2018-07-31 23:41:27 +0800

    first  拉拉阿拉啦

---
+++ src/main/java/com/rice/ricebig0002/Ricebig0002Application.java
@@ -0,0 +1,12 @@
+package com.rice.ricebig0002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ricebig0002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ricebig0002Application.class, args);
+    }
+}
---
+++ src/test/java/com/rice/ricebig0002/Ricebig0002ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rice.ricebig0002;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ricebig0002ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
